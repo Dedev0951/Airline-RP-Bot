9ab80aad55180f8df8c8de8d225a07b289207c6f
Dedev0951 <92547138+Dedev0951@users.noreply.github.com>
2023-09-09 21:09:48 +0200

    Update flightmanager.py

--- flightmanager.py
+++ flightmanager.py
@@ -1,1 +1,35 @@
+import discord
+import json
 
+
+async def hostflight(ctx, flight_number, dest, sched, terminal, gate):
+    with open("flights.json", "w") as f:
+        json.dump(
+            {
+                flight_number: {
+                    "dest": dest,
+                    "sched": sched,
+                    "terminal": terminal,
+                    "gate": gate,
+                }
+            },
+            f,
+        )
+    await ctx.send(f"Flightplan was created! \nFlightnumber: {flight_number}")
+
+
+async def flightinfo(ctx, arg):
+    with open("flights.json", "r") as g:
+        flights = json.load(g)
+    j = flights[arg]
+    Destination = j["dest"]
+    scheduled = j["sched"]
+    Terminal = j["terminal"]
+    Gate = j["gate"]
+    await ctx.send(f"```txt\n\
+                    _________________________________\n\
+                   | Flight | Dest | sched | T | Gt |\n\
+                   |--------------------------------|\n\
+                   | {arg} | {Destination} | {scheduled}z | {Terminal} | {Gate} |\n\
+                   ---------------------------------\n\
+                   ```")
